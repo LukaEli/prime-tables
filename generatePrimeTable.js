33fd6aea4f3f65bce0a4c3e7e3d026d22db3713e
unknown <luka_elizi@yahoo.co.uk>
2026-09-20 17:16:18 +0100

    Add buildGrid function

--- generatePrimeTable.js
+++ generatePrimeTable.js
@@ -41,4 +41,23 @@ function getFirstNPrimes(n) {
     return primes;
 }
 
-console.log(getFirstNPrimes(5));
+/**
+ * Function to build a grid of products of prime numbers.
+ * @param {number[]} primes - An array of prime numbers.
+ * @returns {number[][]} - A 2D array representing the grid of products.
+ */
+function buildGrid(primes) {
+    const grid = [];
+
+    for (let i = 0; i < primes.length; i++) {
+        const row = [];
+        for (let j = 0; j < primes.length; j++) {
+            row.push(primes[i] * primes[j]);
+        }
+        grid.push(row);
+    }
+
+    return grid;
+}
+
+console.log(buildGrid(getFirstNPrimes(3)));

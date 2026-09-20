7aaf9bec1872babd12dfb921e57508c894e0dc78
unknown <luka_elizi@yahoo.co.uk>
2026-09-20 18:04:08 +0100

    Add buildGrid test cases

---
+++ tests/buildGrid.test.js
@@ -0,0 +1,19 @@
+const buildGrid = require('../buildGrid');
+
+test('builds the correct multiplication grid', () => {
+    expect(buildGrid([2, 3, 5])).toEqual([
+        [4, 6, 10],
+        [6, 9, 15],
+        [10, 15, 25]
+    ]);
+});
+
+test('builds a single-cell grid when there is only one prime', () => {
+    expect(buildGrid([2])).toEqual([[4]]);
+});
+
+test('builds a correctly sized grid for a larger set of primes', () => {
+    const result = buildGrid([2, 3, 5, 7]);
+    expect(result.length).toBe(4);
+    expect(result[0].length).toBe(4);
+});

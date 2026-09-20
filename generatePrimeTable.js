f47a0a7eb8ac330c919b112749975d32fef08d51
unknown <luka_elizi@yahoo.co.uk>
2026-09-20 17:40:34 +0100

    Add input validation

--- generatePrimeTable.js
+++ generatePrimeTable.js
@@ -6,6 +6,14 @@ const UserInput = process.argv[2];
 // Convert the input to a number
 const n = Number(UserInput);
 
+// Check if the input is a valid whole number greater than or equal to 1
+// process.exit(1) will terminate the program if the input is invalid
+// process.exit(0) would indicate a successful termination, but we use 1 to indicate an error
+if (isNaN(n) || n < 1) {
+    console.log("Please enter a whole number that is at least 1.");
+    process.exit(1);
+}
+
 const primes = getFirstNPrimes(n);
 const grid = buildGrid(primes);
 console.log(grid);

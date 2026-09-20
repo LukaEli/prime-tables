da41ce8c219432f4707e2519d0d9c4de5570a344
unknown <luka_elizi@yahoo.co.uk>
2026-09-20 17:01:47 +0100

    Implement prime number generation functions

--- generatePrimeTable.js
+++ generatePrimeTable.js
@@ -1,1 +1,31 @@
-console.log("Hello, World!");
+function isPrime(num){
+    if(num <= 1) return false;
+    let divisor = 2;
+
+    
+    while (divisor * divisor <= num) {
+        if (num % divisor === 0) {
+            return false;
+        }
+
+        divisor++;
+    }
+    
+    return true;
+}
+
+function getFirstNPrimes(n) {
+    const primes = [];
+    let num = 2;
+
+    while (primes.length < n) {
+        if (isPrime(num)) {
+            primes.push(num);
+        }
+        num++;
+    }
+
+    return primes;
+}
+
+console.log(getFirstNPrimes(5));
